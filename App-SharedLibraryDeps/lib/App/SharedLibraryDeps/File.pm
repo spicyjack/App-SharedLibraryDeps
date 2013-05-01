163897d0d4937763d070553807e8ece2151fd1f4
Brian Manning <brian@xaoc.org>
2013-05-01 12:47:51 -0700

    Added file object (App:SharedLibraryDeps::File)

---
+++ App-SharedLibraryDeps/lib/App/SharedLibraryDeps/File.pm
@@ -0,0 +1,84 @@
+package App::SharedLibraryDeps::File;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+App::SharedLibraryDeps::File - The great new App::SharedLibraryDeps::File!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::SharedLibraryDeps::File;
+
+    my $foo = App::SharedLibraryDeps::File->new();
+    ...
+
+=head1 OBJECT METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Brian Manning, C<< <xaoc at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+L<https://github.com/spicyjack/App-SharedLibraryDeps/issues>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::SharedLibraryDeps::File
+
+You can also look for information at:
+
+=over 4
+
+=item * Project GitHub repo: acker
+
+L<https://github.com/spicyjack/App-SharedLibraryDeps>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Brian Manning, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of App::SharedLibraryDeps::File

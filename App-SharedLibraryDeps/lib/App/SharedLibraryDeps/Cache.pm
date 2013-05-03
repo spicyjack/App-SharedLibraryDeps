800e0b93acc0ff5f7b8aa7879142e3f3685c5ce3
Brian Manning <brian@xaoc.org>
2013-05-02 17:49:43 -0700

    started working on the file cache object

--- App-SharedLibraryDeps/lib/App/SharedLibraryDeps/Cache.pm
+++ App-SharedLibraryDeps/lib/App/SharedLibraryDeps/Cache.pm
@@ -30,19 +30,84 @@ our $VERSION = '0.01';
 
 =head1 OBJECT ATTRIBUTES
 
-=head2 function1
+=over
+
+=item attribute1
+
+This is some text about attribute #1.
+
+=back
 
 =head1 OBJECT METHODS
 
-=head2 query_ld_so(file => $file)
+=head2 add(file => $file)
+
+Add a file to the cache.  The process of adding a file will cause the Cache
+manager to recursively query C<ld.so> for that file's dependencies; in other
+words, the Cache manager will know the dependencies of the original file, plus
+all of the files that were queried as part of the original file's
+dependencies.
+
+=cut
+
+# for storing file objects once they've been queried via ld.so
+my %_cache;
+
+sub add {
+    my $self = shift;
+    my %args = @_;
+
+    die q|Missing file arg (file => $file)| unless ( exists $args{file} );
+    my $file = $args{file};
+    if ( -r $file ) {
+        # @file_dependencies can be checked to see if the file has already
+        # been cached or not
+        if ( ! exists $_cache{$dependency->filename()} ) {
+            my @file_dependencies = $self->_query_ld_so(file => $file);
+            foreach my $dependency ( @file_dependencies ) {
+                #$self->add(file => $dependency->filename());
+            }
+        }
+    }
+}
+
+=head2 dependencies_for_file(file => $file, order_by => $sort_order)
+
+Return a list of L<App::SharedLibraryDeps::File> objects for the filename
+given as C<$file>, ordered by C<$sort_order>.  C<$sort_order> can be one of
+the following:
+
+=over
+
+=item time_asc
+
+The time the file object was created by the C<Cache> object, ascending.
+
+=item time_desc
+
+The time the file object was created by the C<Cache> object, descending.
+
+=item alpha
+
+Alphabetical by filename and file path
+
+=back
+
+The default sort is C<time_asc>.
+
+=begin internal
+
+=head2 _query_ld_so(file => $file)
 
 Queries the local cache for C<$file>, and the file is not found, then queries
 C</etc/ld.so.cache> via C<ldconfig --print-cache> for file C<$file>, and
 caches it's dependencies as new L<App::SharedLibraryDeps::File> objects.
 
+=end internal
+
 =cut
 
-sub query_ld_so {
+sub _query_ld_so {
     my $self = shift;
     my %args = @_;
 
@@ -50,13 +115,6 @@ sub query_ld_so {
     # FIXME
     # - stat /etc/ld.so.cache and warn if the date is older than say a week
     #   - add an option to the command line to supress the warning
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
 }
 
 =head1 AUTHOR
